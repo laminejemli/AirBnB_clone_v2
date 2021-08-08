83cc8a008cadaffa5e7422bd5efe86c7938ca516
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-08 18:56:08 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -5,12 +5,10 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship, backref
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref='user')
-    reviews = relationship("Review", backref='user')
